18980ac6cdba87f26787722adc25e5af3d7b4151
YongQiangLiu <lyq501@163.com>
2017-04-04 15:40:32 +0800

    add the tensorflow about sensor model and the tensorboard

--- pyprogram/tensorflow_study/tensorflow_sensor_tensorboard.py
+++ pyprogram/tensorflow_study/tensorflow_sensor_tensorboard.py
@@ -28,14 +28,19 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 session = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
+merged_summary_op = tf.summary.merge_all()
+summary_writer = tf.summary.FileWriter('/tmp/mnist_sensor_model.graph', session.graph)
 for i in range(4000):
     batch_xs, batch_ys = minst_data.train.next_batch(100)
-    session.run(train_step, feed_dict={x: batch_xs, y_target: batch_ys})
+    summary = session.run(train_step, feed_dict={x: batch_xs, y_target: batch_ys})
+    summary_writer.add_summary(summary, i)
+
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_target, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(session.run(accuracy, feed_dict={x: minst_data.test.images,
                                   y_target: minst_data.test.labels}))
+
 
 
 
